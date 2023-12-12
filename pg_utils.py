c91b54b4fd00bd85b34dcd30133782c860a61cf3
Zeba-Fathima <121533770+Zeba-Fathima@users.noreply.github.com>
2023-12-12 12:41:38 +0530

    Create pg_utils.py

---
+++ pg_utils.py
@@ -0,0 +1,39 @@
+def open_db():
+    import psycopg2 as pg
+    con = pg.connect(
+        host="localhost",
+        port='5433',
+        database="dbms",
+        user="postgres",
+        password="Nawaz@14")
+    cur = con.cursor()
+    return con, cur
+
+def open_rvuniv():
+    import psycopg2 as pg
+    con = pg.connect(
+        host="localhost",
+        port='5433',
+        database="RVUniv",
+        user="postgres",
+        password="Nawaz@14")
+    cur = con.cursor()
+    return con, cur
+
+def open_lib():
+    import psycopg2 as pg
+    con = pg.connect(
+        host="localhost",
+        port='5433',
+        database="Library",
+        user="postgres",
+        password="Nawaz@14")
+    cur = con.cursor()
+    return con, cur
+
+
+def list_tables(cur):
+    cur.execute("""SELECT table_name FROM information_schema.tables
+       WHERE table_schema = 'public'""")
+    tables = [t[0] for t in cur.fetchall()]
+    return tables
